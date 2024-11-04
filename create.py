5cb440f46d8de21874452ab3bbb8574f1b6a30ca
MrMan314 <mrman314dev@gmail.com>
2024-11-04 11:45:50 -0500

    they did not like the backgorunds :(, update filename, de-blur background, and change text colo(u)r

--- create.py
+++ create.py
@@ -18,8 +18,8 @@ margin_bottom = 100
 
 i = 0
 
-img = ImageEnhance.Brightness(Image.open('backdrop.jpg').filter(ImageFilter.GaussianBlur(5))).enhance(.95)
-
+img = ImageEnhance.Brightness(Image.open('backdrop.jpg')).enhance(.95)
+#.filter(ImageFilter.GaussianBlur(5)))
 blurred = img.filter(ImageFilter.GaussianBlur(52))
 
 draw = ImageDraw.Draw(img)
@@ -29,7 +29,7 @@ font2 = ImageFont.truetype('Comfortaa-VariableFont_wght.ttf', 64)
 font3 = ImageFont.truetype('Comfortaa-VariableFont_wght.ttf', 26)
 
 
-draw.text((width / 2, 175), 'STL 5-Day Weather Forecast' , (255, 255, 255), font = title_font, anchor = "mm", stroke="white", stroke_width=2)
+draw.text((width / 2, 175), 'STL 5-Day Weather Forecast' , (255, 255, 255), font = title_font, anchor = "mm", stroke_width=2)
 draw.text((0, height), ' https://github.com/MrMan314/STLToday-weather-generator', (255, 255, 255), font=font3, anchor="ld")
 
 response = get(f"http://dataservice.accuweather.com/forecasts/v1/daily/5day/49581?apikey={getenv('API_KEY')}&metric=true")
@@ -52,12 +52,12 @@ for day in response.json()['DailyForecasts']:
 
 	img.paste(blurred, mask=mask)
 	draw.text(((width - margin * 2) / columns * (i + 0.5) + margin, 350), datetime.strptime(day['Date'], '%Y-%m-%dT%H:%M:%S%z').strftime('%a') , (255, 255, 255), font = font, anchor = "mm")
-	draw.text(((width - margin * 2) / columns * (i + 0.5) + margin, height - 300), f"H: {round(day['Temperature']['Maximum']['Value'])}°C", (255, 255, 255), font = font2, anchor = "mm")
-	draw.text(((width - margin * 2) / columns * (i + 0.5) + margin, height - 200), f"L: {round(day['Temperature']['Minimum']['Value'])}°C", (255, 255, 255), font = font2, anchor = "mm")
+	draw.text(((width - margin * 2) / columns * (i + 0.5) + margin, height - 300), f"H: {round(day['Temperature']['Maximum']['Value'])}°C", (255, 171, 171), font = font2, anchor = "mm")
+	draw.text(((width - margin * 2) / columns * (i + 0.5) + margin, height - 200), f"L: {round(day['Temperature']['Minimum']['Value'])}°C", (117, 225, 255), font = font2, anchor = "mm")
 	icon = Image.open(f"icons/{str(day['Day']['Icon']).zfill(2)}.png.large")
 	img.paste(icon, (int((width - margin * 2) / columns * (i + 0.5) + margin - icon_width / 2), int((height - icon_height + (font.size - font2.size)/2 + 50) / 2)), icon)
 	i += 1
 
 #draw.rectangle([(192, 108), (1920-192, 1080-108)], outline="red")
 
-img.save('test.png')
+img.save(f'{datetime.now().strftime("%b%d")}_Weather.png')
